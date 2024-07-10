b138cb960e5d271877ec5a9c470f4615072ae75b
Pascal Diehm <pdiehm8@gmail.com>
2024-07-10 12:41:38 +0200

    [meta] Fixed updated commands

--- .config/dotfiles/update.sh
+++ .config/dotfiles/update.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
 
-git --git-dir "$HOME/.config/dotfiles/repo" --work-tree "$HOME" fetch
-git --git-dir "$HOME/.config/dotfiles/repo" --work-tree "$HOME" reset --hard
-git --git-dir "$HOME/.config/dotfiles/repo" --work-tree "$HOME" pull -f origin main
+git --git-dir "$HOME/.config/dotfiles/repo" --work-tree "$HOME" fetch --force origin main
+git --git-dir "$HOME/.config/dotfiles/repo" --work-tree "$HOME" reset --hard origin/main
