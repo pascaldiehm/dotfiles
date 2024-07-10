a682669d49a180dc1d9c434333b89b3e85e7b404
Pascal Diehm <pdiehm8@gmail.com>
2024-07-10 09:52:36 +0200

    [paru] Added configuration

--- .config/dotfiles/root/update.sh
+++ .config/dotfiles/root/update.sh
@@ -3,4 +3,6 @@
 cd $(realpath $(dirname $0))
 sed "s#ExecStart=.*#ExecStart=\"$(pwd)/update.sh\"#" dotfiles-update.service > /etc/systemd/system/dotfiles-update.service
 
+cp ./pacman.conf /etc/pacman.conf
+cp ./paru.conf /etc/paru.conf
 cp ./zshenv /etc/zsh/zshenv
