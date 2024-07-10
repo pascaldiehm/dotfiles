531ae0f5333e44145a21f50f8923a2197513d2c7
Pascal Diehm <pdiehm8@gmail.com>
2024-07-10 14:21:52 +0200

    [reflector] Added configuration

--- .config/dotfiles/system/update.sh
+++ .config/dotfiles/system/update.sh
@@ -6,5 +6,6 @@ sed "s#ExecStart=.*#ExecStart=\"$(pwd)/update.sh\"#" dotfiles-update.service > /
 cp ./locale.gen /etc/locale.gen
 cp ./pacman.conf /etc/pacman.conf
 cp ./paru.conf /etc/paru.conf
+cp ./reflector.conf /etc/xdg/reflector/reflector.conf
 cp ./sddm_kde_settings /etc/sddm.conf.d/kde_settings.conf
 cp ./zshenv /etc/zsh/zshenv
