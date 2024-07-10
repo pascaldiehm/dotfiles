0b09e39c948b8a1b1e4d233c7751cb4140360899
Pascal Diehm <pdiehm8@gmail.com>
2024-07-10 11:36:48 +0200

    [KDE] Added sddm configuration

--- .config/dotfiles/system/update.sh
+++ .config/dotfiles/system/update.sh
@@ -6,4 +6,5 @@ sed "s#ExecStart=.*#ExecStart=\"$(pwd)/update.sh\"#" dotfiles-update.service > /
 cp ./locale.gen /etc/locale.gen
 cp ./pacman.conf /etc/pacman.conf
 cp ./paru.conf /etc/paru.conf
+cp ./sddm_kde_settings /etc/sddm.conf.d/kde_settings.conf
 cp ./zshenv /etc/zsh/zshenv
