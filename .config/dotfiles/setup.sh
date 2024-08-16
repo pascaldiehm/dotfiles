c2cd4cc834d1c44c5c5816223aa9a19a38bd03ee
Pascal Diehm <pdiehm8@gmail.com>
2024-08-16 14:32:47 +0200

    [meta] Added jq and noto fonts extras to setup

--- .config/dotfiles/setup.sh
+++ .config/dotfiles/setup.sh
@@ -28,7 +28,8 @@ paru -S --noconfirm --asdeps perl-yaml-tiny perl-file-homedir
 paru -S --noconfirm networkmanager-openconnect subversion
 paru -S --noconfirm --asdeps perl-term-readkey
 
-paru -S --noconfirm cloc fwupd hexedit htop imagemagick man-db reflector vim xdg-ninja yarn yt-dlp
+paru -S --noconfirm noto-fonts-extra noto-fonts-emoji noto-fonts-cjk
+paru -S --noconfirm cloc fwupd hexedit htop imagemagick jq man-db reflector vim xdg-ninja yarn yt-dlp
 paru -S --noconfirm bitwarden google-chrome signal-desktop thunderbird visual-studio-code-bin vlc
 paru -S --noconfirm docker docker-compose
 paru -S --noconfirm bind traceroute websocat wget wireguard-tools
